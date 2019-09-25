8b1647928b9f0492655c6fa82fffd192e07a30c2
Sohan Bhuiyan <sohanura@gmail.com>
2019-09-25 13:23:55 -0400

    Add map Randomizer
    * randomly changes index value to 1 (blocked) with a 30% chance while the other 70% will be unblocked path

--- src/Path/Generator.py
+++ src/Path/Generator.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Generator:
 
@@ -6,13 +8,26 @@ class Generator:
 
 
     def generateRandomMap(self, x, y, filename):
-        #first want to generate a matrix[x][y]
-        matrix= self.generateMatrix(x,y)
+        matrix= self.__generateMatrix(x,y)
+
+        # randomly selects indices to be the start
+        startX = random.randint(0,x-1)
+        startY = random.randint(0,y-1)
+
+        # randomly selects indices to be the goal
+        goalX = random.randint(0,x-1)
+        goalY = random.randint(0,y-1)
+
+        matrix[startX][startY] = 'S'
+        matrix[goalX][goalY] = 'F'
+
+        self.__randomizeMap(matrix)
+
+        self.__printMatrix(matrix)
 
 
 
-
-    # simply returns a matrix of x by y
+    # simply returns a matrix of x by y. Also init all of them to be 0
     def __generateMatrix(self, x, y):
         n = x
         m = y
@@ -20,3 +35,26 @@ class Generator:
         for row in range(n):
             matrix[row] = [0] * m
         return matrix
+
+    def __printMatrix(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for row in range(rows):
+            print(matrix[row])
+
+    def __randomizeMap(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for row in range(rows):
+            for col in range(cols):
+                # this ensures that it skips over the start & goal states 'S','F'
+                if matrix[row][col] == 0:
+                    # if randomProb > 3 then it is unblocked (this ensures a 70% probability)
+                    randomProbability = random.randint(1,10)
+
+                    if randomProbability <= 3: # 30% chance to get <= 3 out of 10.
+                        matrix[row][col] = 1 # 1 indicates the path is blocked
+        return matrix
+
+
