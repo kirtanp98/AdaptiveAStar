9fc2fe87ca3e9fc81a96c3b7394f45020fd50daf
Sohan Bhuiyan <sohanura@gmail.com>
2019-09-30 14:13:36 -0400

    Remove debugging print statements

--- src/Path/Generator.py
+++ src/Path/Generator.py
@@ -71,9 +71,7 @@ class Generator:
                 if currentState != startState or currentState != goalState: #skip start or goal states
                     # if randomProb > 3 then it is unblocked (this ensures a 70% probability)
                     randomProbability = random.randint(1,10)
-                    print(randomProbability)
                     if randomProbability <= 3:  # 30% chance to get <= 3 out of 10. 30% to block a path
-                        print("Trigg: " + str(randomProbability))
                         currentState.blocked = True  # 1 indicates the path is blocked
         return matrix
 
