08f9945d7e8d2bb9c7d43ee4b854ca1c396c8ae6
Sohan Bhuiyan <sohanura@gmail.com>
2019-10-03 17:50:11 -0400

    Fix

--- src/Path/Decoder.py
+++
@@ -1,9 +0,0 @@
-# will convert textfile to a matrix of the size specified on top
-
-
-class Decoder:
-    def decode(self,filename):
-        f = open(filename, "r")
-
-        line = f.readline()
-        print(line)
