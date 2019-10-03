e96d0c707a586125c346de9d2de74137fe4fdb5f
Sohan Bhuiyan <sohanura@gmail.com>
2019-10-03 17:18:37 -0400

    Implement decode method in Generator
    * able to create a matrix out of the formatted txtfile
    * imported the ReGex library to parse through the txtfile

--- src/path/Generator.py
+++ src/path/Generator.py
@@ -1,4 +1,5 @@
 import random
+import re
 from .Encoder import Encoder
 from .State import State
 
@@ -23,8 +24,8 @@ class Generator:
         encoder = Encoder()
 
         self.__randomizeMap(matrix)
-        encoder.encode(matrix, filename)
-        self.__printMatrix(matrix)
+        encoder.encode(matrix, filename,startX,startY,goalX,goalY)
+        #self.__printMatrix(matrix) // for debugging
         return matrix
 
 
@@ -46,6 +47,7 @@ class Generator:
                 state.yPos = col
         return matrix
 
+    #used for debugging
     def __printMatrix(self, matrix):
         rows = len(matrix)
         cols = len(matrix[0])
@@ -69,5 +71,53 @@ class Generator:
                     if randomProbability <= 3:  # 30% chance to get <= 3 out of 10. 30% to block a path
                         currentState.blocked = True  # 1 indicates the path is blocked
         return matrix
+
+    def decode(self, filename):
+        f = open(filename, "r")
+        line = f.readline()
+        print(line)
+        listOfNumbers = re.findall("[0-9]+",line)
+
+        row = int(listOfNumbers[0])
+        col = int(listOfNumbers[1])
+        startX = int(listOfNumbers[2])
+        startY = int(listOfNumbers[3])
+        goalX = int(listOfNumbers[4])
+        goalY = int(listOfNumbers[5])
+
+        matrix = self.__generateMatrix(row,col)
+
+        lines = f.readlines()
+
+        col = 0
+
+        for i in range(1,row+1): # need to start at 1 cuz lines[0] is blank lines
+            currentLine = lines[i]
+            for j in range(len(currentLine)):
+                if(currentLine[j] != " " and currentLine[j] != "\n" ):
+                    # creating a new State() with the information in the txtfile
+                    state = State()
+                    state.xPos = i-1 # first loop goes from [1,row+1) but we need it to be [0,row)
+                    state.yPos = col
+                    if currentLine[j] == '1':
+                        state.blocked = True
+                    else:
+                        state.blocked = False
+
+                    matrix[i-1][col] = state
+                    col = col+1
+            col = 0 # reset it back to the beginning for the next row
+        return matrix
+
+
+
+
+
+
+
+
+
+
+
 
 
