a05f5d5234f1b1235ba2278913176719cd279fff
Sohan Bhuiyan <chrislostil1@gmail.com>
2019-10-04 11:20:47 -0400

    Show extra info on top of each map

--- src/Path/Encoder.py
+++ src/Path/Encoder.py
@@ -1,11 +1,19 @@
 # will convert matrix to a visual textfile
 
 class Encoder:
-    def encode(self, matrix, filename):
+    def encode(self,matrix, filename,startX,startY,goalX,goalY):
         # need to add a way specify a location
         rows = len(matrix)
         cols = len(matrix[0])
         f = open(filename, "w+")
+
+        # formats the first line of the txtfile
+        f.write(str(rows) +"x" + str(cols) +
+                " start("+str(startX)+","+str(startY)+")" +
+                " goal(" + str(goalX) + "," + str(goalY) + ")" + "\n" )
+        f.write("\n")
+
+
 
         for row in range(rows):
             for col in range(cols):
