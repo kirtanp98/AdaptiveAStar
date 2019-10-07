4e80ecb337f56180e841a6a7f526fe00adbc34fc
Sohan Bhuiyan <chrislostil1@gmail.com>
2019-10-06 23:42:52 -0400

    Way to run the current program

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,25 @@
 import random
-
+from src.Path.Astar import Astar
 from src.Path.Generator import Generator
+from src.Utility.Helper import Helper
 
 if __name__ == '__main__':
     generator = Generator()
-    generator.generateRandomMap(50,50,"") #change dimension here
+   # generator.generateRandomMap(2,2,"test")
+
+    matrix = generator.decode("resources/map3")
+
+    start = generator.startState
+    goal = generator.goalState
+
+    algo = Astar()
+
+    #state = algo.search(matrix,start,goal)
+
+    helper = Helper()
+
+    # creates a solution file with the path image
+    helper.generate_sol_file(matrix,state)
+
+
 
